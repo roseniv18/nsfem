7918f1fc7182f872a2179542cf1e6edb2f5eeac5
roseniv18 <roseniv18@yahoo.com>
2026-09-03 10:40:53 +0300

    add tests for local stiffness matrix

---
+++ tests/local_stiffness_matrix_test.cpp
@@ -0,0 +1,67 @@
+#include <gtest/gtest.h>
+#include "assemble/assemble.h"
+
+std::vector<Node> nodes{{1, 0.0, 0.0}, {2, 1.0, 0.0}, {3, 0.0, 1.0}};
+Element element{.dim = 2,
+                .element_tag = 1,
+                .type = ElementType::Triangle3,
+                .physical_tags = {},
+                .node_indices = {0, 1, 2}};
+
+const TriangleGEO ref_triangle(element, nodes);
+
+const auto ls_matrix = generate_ls_matrix(ref_triangle);
+
+/** Local stiffness matrix for reference triangle
+ * Test if the local stiffness matrix for the reference triangle
+ * is equal to [[1,-1/2,-1/2], [-1/2,1/2,0], [-1/2,0,1/2]]
+ */
+TEST(LocalStiffnessTest, ReferenceTriangle) {
+  EXPECT_DOUBLE_EQ(ls_matrix(0, 0), 1.0);
+  EXPECT_DOUBLE_EQ(ls_matrix(1, 0), -0.5);
+  EXPECT_DOUBLE_EQ(ls_matrix(2, 0), -0.5);
+  EXPECT_DOUBLE_EQ(ls_matrix(0, 1), -0.5);
+  EXPECT_DOUBLE_EQ(ls_matrix(1, 1), 0.5);
+  EXPECT_DOUBLE_EQ(ls_matrix(2, 1), 0.0);
+  EXPECT_DOUBLE_EQ(ls_matrix(0, 2), -0.5);
+  EXPECT_DOUBLE_EQ(ls_matrix(1, 2), 0.0);
+  EXPECT_DOUBLE_EQ(ls_matrix(2, 2), 0.5);
+}
+
+/** Local stiffness matrix for physical triangle
+ * Test if the local stiffness matrix for the physical triangle
+ * with nodes (1, 2), (4,2), (1, 6)
+ * is equal to [[25/24, -2/3, -3/8], [-2/3, 2/3, 0], [-3/8, 0, 3/8]]
+ */
+TEST(LocalStiffnessTest, PhysTriangle) {
+  std::vector<Node> phys_nodes{{1, 1.0, 2.0}, {2, 4.0, 2.0}, {3, 1.0, 6.0}};
+  Element phys_element{.dim = 2,
+                       .element_tag = 1,
+                       .type = ElementType::Triangle3,
+                       .physical_tags = {},
+                       .node_indices = {0, 1, 2}};
+
+  const TriangleGEO phys_triangle(phys_element, phys_nodes);
+
+  const auto phys_ls_matrix = generate_ls_matrix(phys_triangle);
+
+  EXPECT_NEAR(phys_ls_matrix(0, 0), 25.0 / 24.0, 1e-12);
+  EXPECT_NEAR(phys_ls_matrix(1, 0), -2.0 / 3.0, 1e-12);
+  EXPECT_NEAR(phys_ls_matrix(2, 0), -3.0 / 8.0, 1e-12);
+  EXPECT_NEAR(phys_ls_matrix(0, 1), -2.0 / 3.0, 1e-12);
+  EXPECT_NEAR(phys_ls_matrix(1, 1), 2.0 / 3.0, 1e-12);
+  EXPECT_NEAR(phys_ls_matrix(2, 1), 0.0, 1e-12);
+  EXPECT_NEAR(phys_ls_matrix(0, 2), -3.0 / 8.0, 1e-12);
+  EXPECT_NEAR(phys_ls_matrix(1, 2), 0.0, 1e-12);
+  EXPECT_NEAR(phys_ls_matrix(2, 2), 3.0 / 8.0, 1e-12);
+}
+
+/** Local stiffness matrix for reference triangle
+ * Test if the local stiffness matrix for the reference triangle
+ * is symmetric K=K^T
+ */
+TEST(LocalStiffnessTest, IsSymmetric) {
+  EXPECT_NEAR(ls_matrix(0, 1), ls_matrix(1, 0), 1e-12);
+  EXPECT_NEAR(ls_matrix(0, 2), ls_matrix(2, 0), 1e-12);
+  EXPECT_NEAR(ls_matrix(1, 2), ls_matrix(2, 1), 1e-12);
+}
